0a65c58ed28c49e8e5ca3d51625ebb2abca827e5
XiZhao <kwang1imsa@gmail.com>
2015-03-22 04:10:01 -0700

    spec out initial app

--- dispatch/src/js/index.js
+++ dispatch/src/js/index.js
@@ -11,7 +11,23 @@ var DefaultRoute = Router.DefaultRoute;
 var Link = Router.Link;
 var RouteHandler = Router.RouteHandler;
 
+var DS = {
+	_cache: {},
+	set: function(url, val) {
+		DS._cache[url] = val;
+	},
+	get: function(url, cb) {
+		if(DS._cache[url]) {
+			return cb(DS._cache[url]);
+		}
+		return $.get('/api/' + url, cb);
+	}
+}
+
 var Dashboard = React.createClass({
+	contextTypes: {
+    router: React.PropTypes.func.isRequired
+  },
   render: function() {
     return (
     	<div className="dashboard">
@@ -19,7 +35,7 @@ var Dashboard = React.createClass({
     			<h1>Leapmates Order</h1>
     		</div>
    			<div className="container">
-   				<LocationList/>
+   				<RouteHandler/>
    			</div>
   		</div>
   	);
@@ -34,7 +50,24 @@ var NotFound = React.createClass({
 	}
 });
 
+var LocationPage = React.createClass({
+	contextTypes: {
+    router: React.PropTypes.func.isRequired
+  },
+	render: function() {
+		return (
+			<div>
+				<h2 className="text-center">Choose a Location:</h2>
+				<LocationList/>
+			</div>
+		);
+	}
+});
+
 var LocationList = React.createClass({
+	contextTypes: {
+    router: React.PropTypes.func.isRequired
+  },
 	getInitialState: function() {
     return { 
     	loading: true,
@@ -43,12 +76,17 @@ var LocationList = React.createClass({
   },
   componentDidMount: function() {
   	var self = this;
-    $.get('/api/locations', function(data) {
+    DS.get('locations', function(data) {
       self.setState({
       	loading: false,
       	locations: data
       });
     });
+  },
+  componentDidUpdate: function() {
+  	for(var i=0; i<this.state.locations; i++) {
+  		DS.set('locations/' + this.state.locations[i].id, this.state.locations[i]);
+  	}
   },
 	render: function() {
 		var locations = [];
@@ -56,38 +94,100 @@ var LocationList = React.createClass({
 			return <div>Loading...</div>;
 		}
 		_.each(this.state.locations, function(loc) {
-			locations.push(<Location/>);
+			locations.push(<LocationListItem key={loc.id} initId={loc.id} initName={loc.name} initLocation={loc.location} initImages={loc.images}/>);
 		});
 		if(locations.length <= 0) {
 			return <div>No Locations Found</div>;
 		}
 		return (
-			<ul className="location_list">{locations}</ul>
+			<div className="location_list">
+				<ul>{locations}</ul>
+			</div>
 		);
 	}
 });
 
-var Location = React.createClass({
+var LocationListItem = React.createClass({
+	contextTypes: {
+    router: React.PropTypes.func.isRequired
+  },
+	getInitialState: function() {
+		return {
+			id: this.props.initId,
+			name: this.props.initName,
+			baseImgUrl: "http://leapset-superadmin-s3.leapset.com/",
+			location: this.props.initLocation,
+			address: null,
+			deliveryFee: 0.0,
+			minimumOrderValue: 0.0,
+			minDeliveryOrderAmount: 0.0,
+			queueConsumers: 1,
+			images: this.props.initImages,
+			openNow: true,
+			timeZone: null,
+			paypalAvailable: false
+		};
+	},
 	render: function() {
+		var itemStyle = {
+		  backgroundImage: 'url(' + this.state.images.image[0].resourceUri + ')'
+		};
 		return (
-			<li className="location"></li>
+			<li className="location" style={itemStyle}><a href={'/locations/' + this.state.id}>{this.state.name}</a></li>
 		);
 	}
 });
 
-React.render(<Dashboard/>, document.getElementById('root'));
+var MenuPage = React.createClass({
+	contextTypes: {
+    router: React.PropTypes.func.isRequired
+  },
+	getInitialState: function() {
+		return {
+			loading: true,
+			location: null,
+			menu: null
+		};
+	},
+	componentDidMount: function() {
+		var self = this,
+				loc_id = this.context.router.getCurrentParams().location_id;
+		DS.get('locations/' + loc_id, function(loc) {
+			DS.get('menus/' + loc_id, function(menu) {
+				self.setState({
+					loading: false,
+					location: loc,
+					menu: menu
+				});
+			});
+		});
+	},
+	render: function() {
+		if(this.state.loading) {
+			return <div>Loading...</div>;
+		}
+		return (
+			<div className="menu">
+				<h2>{this.state.location.name}</h2>
+				<div className="row">
+					<div className="col-md-8">ads</div>
+					<div className="col-md-4">asda</div>
+				</div>
+			</div>
+		);
+	}
+});
 
 // Routing
 
 var routes = (
-  <Route>
-  	<Route name="home" path="/" handler={Dashboard}>
-
-  	</Route>
+  <Route handler={Dashboard} path="/">
+	  <DefaultRoute handler={LocationList}/>
+		<Route name="menu" path="locations/:location_id" handler={MenuPage}/>
 	  <NotFoundRoute handler={NotFound}/>
   </Route>
 );
 
 Router.run(routes, Router.HistoryLocation, function (Handler) {
-  React.render(<Handler/>, document.body);
+  React.render(<Handler/>, document.getElementById('root'));
 });
